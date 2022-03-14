2619e79797ab2b172c8ffe3296ce2f619077c13e
François Rullière <rulliere.francois@gmail.com>
2022-03-14 22:12:54 +0800

    add url

--- src/App.svelte
+++ src/App.svelte
@@ -5,29 +5,34 @@
 	import months from "./data/months.json";
 	import saints from "./data/saints.json";
 	import { toRep } from "./compute";
+	import url from "./url";
 	const DEF_TIME = "T00:00:00.001Z";
-	let [date, time] = new Date().toISOString().split("T");
+	$: urlDate = dayjs(($url as any).hash.replace(/^#\//, "").concat(DEF_TIME));
+	$: dateTime = urlDate.isValid() ? urlDate : dayjs();
+	$: [date, time] = dateTime.toISOString().split("T");
 	export const days = new Array(10).fill(null).map((_, i) => i + 1);
 	export const decades = new Array(3).fill(null).map((_, i) => i);
 	$: year = dayjs(date + DEF_TIME).get("year");
 	$: rep = toRep(date); // Number of day since the begining of the Republican year
 	$: formated = rep.format();
-	$: console.log(formated);
-	function setRep(e: any) {
-		const repDayNb = e.target.id.split("-")[1];
-		date = dayjs(year + "-09-22" + DEF_TIME)
-			.add(repDayNb - 1, "days")
+	$: console.log({ date });
+	function toGreg(d: number) {
+		return dayjs(year + "-09-22" + DEF_TIME)
+			.add(d - 1, "days")
 			.toISOString()
 			.split("T")[0];
-		console.log("Setting date", repDayNb, { date });
+	}
+	function setGreg(e: any) {
+		const repDayNb = e.target.id.split("-")[1];
+		date = toGreg(repDayNb);
 	}
 	function dayCount(day: number, decade: number, monthIndex: number) {
 		return monthIndex * 30 + 10 * decade + day;
 	}
 </script>
 
-<main class=" px-4">
-	<h1 class="text-3xl text-center">Calendrier Républicain Pratique</h1>
+<main class="px-4">
+	<h1 class="text-3xl text-center">Calendrier Républicain Moderne</h1>
 	<div class="container mx-auto p-4">
 		Ce calendrier est inspiré du <a
 			href="https://fr.wikipedia.org/wiki/Calendrier_r%C3%A9publicain"
@@ -36,7 +41,7 @@
 		en imposant le premier jour de l'année au 22 Septembre afin de coïncider avec
 		les années bisextiles du calendrier Grégorien. Ainsi ce calendrier
 		<b>ne correspond pas au calendrier républicain de 1791-1805</b> mais représente
-		une version contemporaine, alternative au calendrier grégorien.
+		une version moderne, alternative au calendrier grégorien.
 	</div>
 	<div>
 		<label for="date-greg">Date calendrier Grégorien: </label>
@@ -73,13 +78,14 @@
 						{#each months as _, mi}
 							<td
 								id="day-{mi * 30 + 10 * de + d}"
-								on:click={setRep}
 								class="border-r 
 									{!((mi % 3) - 2) ? 'border-r-black' : ''} 
 									{dayCount(d, de, mi) === formated.dayNumber ? 'bg-blue-400' : ''}
 								"
 							>
-								{saints[dayCount(d, de, mi) - 1]}
+								<a href="/#/{toGreg(dayCount(d, de, mi))}">
+									{saints[dayCount(d, de, mi) - 1]}
+								</a>
 							</td>
 						{/each}
 					</tr>
@@ -101,9 +107,11 @@
 					<td
 						class="border 
 						{360 + i + 1 === formated.dayNumber ? 'bg-blue-400' : ''}"
-						id="day-{360 + i + 1}"
-						on:click={setRep}>{f}</td
 					>
+						<a href="/#/{toGreg(360 + i + 1)}">
+							{f}
+						</a>
+					</td>
 				{/each}
 			</tr>
 		</tbody>
